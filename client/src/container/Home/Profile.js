907c3c25fef36ae22b463c1639bcaba27d58e4c3
George Cheng <sparklyspuds@gmail.com>
2022-05-07 17:28:32 +0800

    import bootstrap

--- client/src/container/Home/Profile.js
+++ client/src/container/Home/Profile.js
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-container">
+      Profile
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="#">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
